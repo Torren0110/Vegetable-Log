00e10657ed688be4dfae1dd3b054e7e9b794f76d
Yashasvi Rana <yashasvi.0110@gmail.com>
2023-08-05 19:35:56 +0530

    made search bar

--- Vegetable-Logs/src/components/Catalogue/Catalogue.jsx
+++ Vegetable-Logs/src/components/Catalogue/Catalogue.jsx
@@ -1,14 +1,15 @@
+import { useState } from "react";
 import useVegetable from "../../hooks/useVegetable";
-import VegetableCard from "./VegetableCard";
 import VegetableGrid from "./VegetableGrid";
 import VegetableSearchBar from "./VegetableSearchBar";
 
 const Catalogue = () => {
-  const { vegetables } = useVegetable();
+  const [ searchString, setSearchString ] = useState("");
+  const { vegetables, setVegetables } = useVegetable();
 
   return (
     <div>
-      <VegetableSearchBar />
+      <VegetableSearchBar onSearch = {(str) => { console.log(str) }} />
       <VegetableGrid vegetables={vegetables} />
     </div>
   );
--- Vegetable-Logs/src/components/Catalogue/VegetableSearchBar.jsx
+++ Vegetable-Logs/src/components/Catalogue/VegetableSearchBar.jsx
@@ -1,4 +1,10 @@
-const VegetableSearchBar = () => {
+import { useState } from "react";
+
+
+const VegetableSearchBar = ({ onSearch }) => {
+  const [ search, setSearch ] = useState("");
+
+
   return (
     <div className="w-100 d-flex justify-content-center my-4">
       <input
@@ -6,8 +12,9 @@ const VegetableSearchBar = () => {
         type="search"
         placeholder="Search"
         aria-label="Search"
+        onChange={(event) => { setSearch(event.target.value) }}
       />
-      <button className="btn btn-outline-primary" type="submit">
+      <button className="btn btn-outline-primary" type="submit" onClick={() => onSearch(search) }>
         Search
       </button>
     </div>
