7a5f5768cc492d3ec1721550173860e76eed6d41
Yashasvi Rana <yashasvi.0110@gmail.com>
2023-08-07 17:19:20 +0530

    installed zod, react-hook-forms, create sell form

--- Vegetable-Logs/src/App.jsx
+++ Vegetable-Logs/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Catalogue from './components/Catalogue/Catalogue'
+import SellForm from './components/SellForm/SellForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Catalogue />
+      {/* <Catalogue /> */}
+      <SellForm />
     </>
   )
 }
