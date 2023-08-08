4cd1c2bb5a23c8ac2c7d2f02f6fa25ddebff465f
Harshit Bajpai <rogeralpha700@gmail.com>
2023-08-08 21:28:14 +0530

    added Cart functions

--- Vegetable-Logs/src/components/Cart/Cart.jsx
+++ Vegetable-Logs/src/components/Cart/Cart.jsx
@@ -1,11 +1,14 @@
 import React,{useContext} from 'react'
+import {useNavigate} from 'react-router-dom'
 import { ShopContext } from '../../context/shop-context'
 import CartItem from './CartItem';
 import './Cart.css'
 
 const Cart = () => {
 
-    const {products, cartItems} = useContext(ShopContext);
+    const {products, cartItems, getTotalCartAmount} = useContext(ShopContext);
+    const totalAmt = getTotalCartAmount();
+    const navigate = useNavigate()
 
   return (
     <div className='cart' >
@@ -14,11 +17,19 @@ const Cart = () => {
       </div>
       <div className="cartItems">
         {products.map((prod)=>{
-            if(cartItems[prod.id] !==0 ){
-                return <CartItem data={prod} />
+            if(cartItems[prod.id] !== 0 ){
+                return <CartItem key={prod.id} data={prod} /> 
             }
         })}
       </div>
+      {totalAmt>0 ?
+      <div className="checkout">
+        <p>Subtotal: Rs. {totalAmt}</p>
+        <button onClick={()=>navigate('/')} >Continue Shopping</button>
+        <button>Checkout</button>
+      </div>:
+      <h1>Your Cart is Empty!!</h1>
+      }
     </div>
   )
 }
--- Vegetable-Logs/src/context/shop-context.jsx
+++ Vegetable-Logs/src/context/shop-context.jsx
@@ -14,23 +14,46 @@ export const ShopContext = createContext(null);
 
 const getDefaultCart=()=>{
     let cart = {}
-    for(let i=1; products.length+1; i++){
+    for(let i=1; i<products.length+1; i++){
         cart[i] = 0;        
     }
+    cart[1]=1;
+    cart[2]=2;
     return cart;
 }
 
 export const ShopContextProvider = (props) =>{
-    const [cartItems, setCartItems] = useState()
+    const [cartItems, setCartItems] = useState(getDefaultCart())
 
     const addToCart = (Itemid)=>{
         setCartItems((prev)=> ({...prev, [Itemid]: prev[Itemid]+1 }))
     }
+
     const removeFromCart = (Itemid)=>{
         setCartItems((prev)=> ({...prev, [Itemid]: prev[Itemid]-1 }))
     }
 
-    const contextValue = {cartItems, addToCart, removeFromCart, products}
+    const updateCartItemCount = (newAmt, Itemid)=>{
+        setCartItems((prev)=>({...prev, [Itemid]:newAmt}))
+    }
+
+    const getTotalCartAmount = ()=>{
+        let total=0;
+        for(const item in cartItems){
+            if(cartItems[item] > 0){
+                let itemInfo = products.find((product)=>product.id === Number(item));
+                total += cartItems[item] * itemInfo.price; 
+            }
+        }
+        return total;
+    }
+
+    const contextValue = {products,
+                        cartItems,
+                        addToCart,
+                        removeFromCart,
+                        updateCartItemCount,
+                        getTotalCartAmount}
 
     return <ShopContext.Provider value={contextValue} >{props.children}</ShopContext.Provider>
 }
