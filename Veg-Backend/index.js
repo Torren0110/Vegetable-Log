584e4f642594cedd2dd03c6f7be285dd12cf2330
Yashasvi Rana <yashasvi.0110@gmail.com>
2023-08-09 23:01:13 +0530

    created user model, user endpoints

--- Veg-Backend/index.js
+++ Veg-Backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const vegetables = require("./routes/vegetables");
+const users = require("./routes/users");
 const mongoose = require("mongoose");
 const app = express();
 
@@ -12,6 +13,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use("/api/vegetables", vegetables);
+app.use("/api/users", users);
 
 const port = 3000;
 
---
+++ Veg-Backend/models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: { type: String, minlength: 5, required: true, unique: true },
+  email: {
+    type: String,
+    validate: {
+      validator: function (value) {
+        return /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(value);
+      },
+      message: "Invalid email",
+    },
+    unique: true,
+  },
+  password: { type: String, minlength: 8, required: true },
+});
+
+const User = new mongoose.model("User", userSchema);
+
+module.exports = User;
