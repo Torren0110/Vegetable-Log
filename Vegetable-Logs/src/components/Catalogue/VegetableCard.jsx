b3e0b0c71aa4a3aaf35664dd9487eafd5edafd93
Yashasvi Rana <yashasvi.0110@gmail.com>
2023-08-10 21:25:03 +0530

    fixed item grid

--- Vegetable-Logs/src/components/Catalogue/VegetableCard.jsx
+++ Vegetable-Logs/src/components/Catalogue/VegetableCard.jsx
@@ -1,10 +1,18 @@
 import "./style.css";
 import logo from "../../assets/logo.webp";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+} from "@mui/material";
 
 const VegetableCard = ({ vegetable }) => {
+
   return (
-<<<<<<< HEAD
     <>
       {/* <div className="card mx-2 mb-4">
         <img
@@ -48,21 +56,7 @@ const VegetableCard = ({ vegetable }) => {
         </Card>
       </Grid>
     </>
-=======
-    <div className="card mx-2 mb-4" onClick={() => {console.log(vegetable._id)}}>
-      <img src={ vegetable.image ? vegetable.image : logo} className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">{vegetable.name}</h5>
-        <p className="card-text">
-          Price: {vegetable.price}
-          {/* img: {vegetable.image} */}
-        </p>
-        <Link to={`/itemview/${vegetable._id}`} className="btn btn-primary">
-          View Veggie
-        </Link>
-      </div>
-    </div>
->>>>>>> dc69de64c291cb0d6861df32cf6ab446441169c8
   );
 };
+
 export default VegetableCard;
