e3e228c9f4d0839fd45f4227062f5426b5539051
Yashasvi Rana <yashasvi.0110@gmail.com>
2023-08-10 09:11:37 +0530

    made endpoint for cart

--- Veg-Backend/routes/carts.js
+++ Veg-Backend/routes/carts.js
@@ -5,33 +5,48 @@ const User = require("../models/userModel");
 const Vegetable = require("../models/vagetableModel");
 const Cart = require("../models/cartModel");
 
-router.get("/", (req, res) => {
-    res.json(req.body);
-})
+router.get("/", async (req, res) => {
+  const uid = req.body.uid;
+
+	if(!uid) return res.status(400).send("User id not present");
+
+  try {
+		const cartItems = await Cart.find({ userID: uid }).select(["userID", "vegID"]);
+		let cart = [];
+
+		cartItems.forEach(async (item) => {
+			const veg = await Vegetable.find({_id: item.vegID}).select(["name", "price"]);
+			cart.push({ ...item, vegetable: veg });
+		});
+		
+    res.json(cart);
+  } catch(err) {
+		res.status(400).send("Something went wrong");
+	}
+});
 
 router.post("/", async (req, res) => {
-    const uid = req.body.uid;
-    const vid = req.body.vid;
-    const quantity = parseInt(req.body.quantity);
-    
-    if(!uid || !vid || !quantity || quantity <= 0) return res.status(400).send("Invalid request");
+  const uid = req.body.uid;
+  const vid = req.body.vid;
+  const quantity = parseInt(req.body.quantity);
 
-    try {
-        const user = await User.findOne({ _id: uid });
-        let veg = await Vegetable.findOne({ _id: vid });
+  if (!uid || !vid || !quantity || quantity <= 0)
+    return res.status(400).send("Invalid request");
 
-        if(veg.quantity < quantity) throw new Error("Invalid Quantity");
+  try {
+    const user = await User.findOne({ _id: uid });
+    let veg = await Vegetable.findOne({ _id: vid });
 
-        let cartItem = new Cart({ userID: uid, vegID: vid, quantity: quantity });
-        veg.quantity -= quantity;
-        veg = veg = await veg.save();
-        cartItem = await cartItem.save();
-        res.json(cartItem);
-    }
-    catch(err) {
-        res.status(400).send(err.message);
-    }
+    if (veg.quantity < quantity) throw new Error("Invalid Quantity");
 
-})
+    let cartItem = new Cart({ userID: uid, vegID: vid, quantity: quantity });
+    veg.quantity -= quantity;
+    veg = veg = await veg.save();
+    cartItem = await cartItem.save();
+    res.json(cartItem);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
 
 module.exports = router;
