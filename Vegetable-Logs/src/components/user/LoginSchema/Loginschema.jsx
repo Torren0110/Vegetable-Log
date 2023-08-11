914874ffc029c7c174ef9ab44a01657e8997606a
Sagar Negi <negis2673@gmail.com>
2023-08-11 17:17:13 +0530

    made changes in certain pages

--- Vegetable-Logs/src/components/user/LoginSchema/Loginschema.jsx
+++ Vegetable-Logs/src/components/user/LoginSchema/Loginschema.jsx
@@ -1,7 +1,13 @@
 import * as Yup from "yup";
 
 export const loginschema = Yup.object({
-  email: Yup.string().email().required("Please enter your email"),
+  username: Yup
+  .string()
+  .required('Username is required')
+  .min(6, 'Username must be at least 6 characters')
+  .max(20, 'Username must be at most 20 characters')
+  .matches(/^[a-zA-Z0-9_]+$/, 'Username can only contain letters, numbers, and underscores'),
+  // email: Yup.string().email().required("Please enter your email"),
   password: Yup.string().min(6).required("Please enter your password"),
   
 });
